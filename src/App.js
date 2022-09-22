1f04b5c14c3e416334e2bc43af232e317e1f6b46
joaquinsantos0715 <50461000+joaquinsantos0715@users.noreply.github.com>
2022-09-22 14:04:22 -0400

    Commit momenta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/styles.scss';
+import Curso from './Cursos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>(
+<>
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="banner" src="https://wallpapercave.com/wp/wp5430320.jpg" />
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Cursos React</p>
+        <p>Tu curso te espera</p>
+        <a href="https://ed.team" className="button">Suscribirse</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+<Curso />
+<Curso />
+<Curso />
+<Curso />
+</div>
+</>
+) 
 
 export default App;
