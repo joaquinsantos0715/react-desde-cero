97648bf86886015f75234fb2abdca92e9e663bc7
joaquinsantos0715 <50461000+joaquinsantos0715@users.noreply.github.com>
2022-09-23 10:42:02 -0400

    Commit agregando arreglo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from 'react';
 import './styles/styles.scss';
 import Curso from './Cursos';
 
+const Cursos = [
+  {
+    "title":"React desde cero",
+    "image": "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fmedium%2Fb4b80bd3-4fc0-45f1-946c-a1386a8e8221.png&w=1920&q=75",
+    "price":  50,
+    "profesor":"Joaquin Santos"
+  },{
+    "title":"React desde cero",
+    "image": "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fmedium%2Fb4b80bd3-4fc0-45f1-946c-a1386a8e8221.png&w=1920&q=75",
+    "price":  50,
+    "profesor":"Joaquin Santos"
+  },{
+    "title":"React desde cero",
+    "image": "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fmedium%2Fb4b80bd3-4fc0-45f1-946c-a1386a8e8221.png&w=1920&q=75",
+    "price":  50,
+    "profesor":"Joaquin Santos"
+  },{
+    "title":"React desde cero",
+    "image": "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fmedium%2Fb4b80bd3-4fc0-45f1-946c-a1386a8e8221.png&w=1920&q=75",
+    "price":  50,
+    "profesor":"Joaquin Santos"
+  }
+]
+
+
 const App = () =>(
 <>
 <div className="main-banner img-container l-section" id="main-banner">
@@ -9,7 +34,7 @@ const App = () =>(
     <div className="lg-cols-4 lg-x-2">
       <img className="main-banner__img" alt="banner" src="https://wallpapercave.com/wp/wp5430320.jpg" />
       <div className="main-banner__data s-center">
-        <p className="t2 s-mb-0">Cursos React</p>
+        <p className="t2 s-mb-0">Cursos React Enfoco srl</p>
         <p>Tu curso te espera</p>
         <a href="https://ed.team" className="button">Suscribirse</a>
       </div>
@@ -18,10 +43,9 @@ const App = () =>(
 </div>
 
 <div className="ed-grid m-grid-3">
-<Curso />
-<Curso />
-<Curso />
-<Curso />
+{
+  Cursos.map( c => <Curso title={c.title} image={c.image} price={c.price} profesor={c.profesor}  />)
+}
 </div>
 </>
 ) 
--- src/Cursos.jsx
+++ src/Cursos.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types"
 
-const Curso = () =>(
-    <article className="card">
+const Curso = ({title, image, price, profesor}) =>(
+   <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2Fb12a78d6-cbcc-4cb3-a8d2-02b7df099efa.jpg&w=1920&q=75" alt="paython" />
+        <img src={image} alt={title} />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-        </h3>
-        <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-                <img src="https://edteam-media.s3.amazonaws.com/users/avatar/0b5fec83-84c8-4651-8984-a71f18bcf286.jpg" alt="Profesor" />
-            </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-        </div>
-        </div>
-        <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-        </div>
+      <h3 className="center">{title}</h3>
+      <div className="s-main-center">
+        {`Prof.: ${profesor}`}
+      </div>
+      <div className="s-main-center">     
+            <a className="button--ghost-alert button--tiny" href="#">{price}</a>
+       </div>
     </div>
-    </article>
+   </article>
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+    title: "No se encotro Titulo",
+    image: "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2Fd1567356-5b38-432b-a1c9-f135e41b8bbc.png&w=1920&q=75",
+    price: "---",
+    profesor: ""
+}
 
 export default Curso
